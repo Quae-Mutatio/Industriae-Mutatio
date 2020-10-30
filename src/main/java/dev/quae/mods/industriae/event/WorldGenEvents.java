c58f4e9a2da8ada35e5f5e93c74e8210a722ff4a
Luke Gilfoyle <boo122333@gmail.com>
2020-10-30 10:07:48 +0000

    Fix the removal of vanilla ores
    This is done in a really janky way, might need to change how this is done in the future, when it is more clear how to remove features.
    
    Signed-off-by: Luke Gilfoyle <boo122333@gmail.com>

--- src/main/java/dev/quae/mods/industriae/event/WorldGenEvents.java
+++ src/main/java/dev/quae/mods/industriae/event/WorldGenEvents.java
@@ -3,10 +3,14 @@ package dev.quae.mods.industriae.event;
 import com.google.common.collect.ImmutableSet;
 import dev.quae.mods.industriae.worldgen.OreFeatures;
 import java.util.Set;
+import java.util.stream.Collectors;
 import net.minecraft.world.biome.Biome.Category;
 import net.minecraft.world.gen.GenerationStage.Decoration;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.DecoratedFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -42,6 +46,17 @@ public class WorldGenEvents {
         || event.getCategory().equals(Category.NONE)) {
       return;
     }
-    event.getGeneration().getFeatures(Decoration.UNDERGROUND_ORES).removeIf(feature -> BLACKLIST.contains(feature.get()));
+    final Set<Feature<?>> oreFeatures = BLACKLIST.stream().map(WorldGenEvents::getRealFeature).collect(Collectors.toSet());
+    event.getGeneration().getFeatures(Decoration.UNDERGROUND_ORES).removeIf(f -> oreFeatures.contains(getRealFeature(f.get())));
+  }
+
+  static Feature<?> getRealFeature(ConfiguredFeature<?, ?> feature) {
+    ConfiguredFeature<?, ?> CF = feature;
+    IFeatureConfig C = CF.config;
+    while (C instanceof DecoratedFeatureConfig) {
+      CF = ((DecoratedFeatureConfig) C).feature.get();
+      C = CF.config;
+    }
+    return CF.feature;
   }
 }
