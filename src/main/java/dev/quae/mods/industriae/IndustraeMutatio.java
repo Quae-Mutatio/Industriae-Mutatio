3ec71adaadcd8ec53257e77d1250c1ef17450a67
KyleBCox <kylecoxforweb@gmail.com>
2021-02-25 22:29:32 +0000

    initial project setup

---
+++ src/main/java/dev/quae/mods/industriae/IndustraeMutatio.java
@@ -0,0 +1,12 @@
+package dev.quae.mods.industriae;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(IndustraeMutatio.ID)
+public class IndustraeMutatio {
+    public static final String ID = "industriaemutatio";
+
+    public IndustraeMutatio() {
+        
+    }
+}
